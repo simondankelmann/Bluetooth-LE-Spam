9db9f803459cbbec6c2883d5740d89f0d3388634
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2023-11-04 01:42:21 +0100

    Match `compileSdk` with `targetSdk`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "de.simon.dankelmann.bluetoothlespam"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
