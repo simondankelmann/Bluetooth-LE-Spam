7a6acfcd24681d2670e9205c0808b1875cb79356
Simon <simon.dankelmann@gmail.com>
2023-11-05 19:36:28 +0100

    increased version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
