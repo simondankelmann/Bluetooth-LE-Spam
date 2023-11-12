6ad3e89f89754dd2ba9722aebab1dae22b24501f
Simon Dankelmann <simon.dankelmann@gmail.com>
2023-11-12 13:49:14 +0100

    continued ui improvements

--- app/src/main/java/de/simon/dankelmann/bluetoothlespam/ui/start/StartFragment.kt
+++ app/src/main/java/de/simon/dankelmann/bluetoothlespam/ui/start/StartFragment.kt
@@ -1,14 +1,28 @@
 package de.simon.dankelmann.bluetoothlespam.ui.start
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import de.simon.dankelmann.bluetoothlespam.R
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import de.simon.dankelmann.bluetoothlespam.AppContext.AppContext
+import de.simon.dankelmann.bluetoothlespam.databinding.FragmentStartBinding
+
 
 class StartFragment : Fragment() {
+
+    private val _logTag = "StartFragment"
+    private var _viewModel: StartViewModel? = null
+    private var _binding: FragmentStartBinding? = null
+
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
 
     companion object {
         fun newInstance() = StartFragment()
@@ -16,11 +30,63 @@ class StartFragment : Fragment() {
 
     private lateinit var viewModel: StartViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_start, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val viewModel = ViewModelProvider(this)[StartViewModel::class.java]
+        _viewModel = viewModel
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        _viewModel!!.appVersion.postValue(getAppVersion())
+        _viewModel!!.androidVersion.postValue(android.os.Build.VERSION.RELEASE)
+        _viewModel!!.sdkVersion.postValue(android.os.Build.VERSION.SDK_INT.toString())
+        _viewModel!!.bluetoothSupport.postValue(getBluetoothSupportText())
+
+        setupUi()
+        return root
+    }
+
+    fun getAppVersion():String{
+        val manager = AppContext.getContext()!!.packageManager
+        val info = manager.getPackageInfo(AppContext.getContext().packageName, 0)
+        val version = info.versionName
+        return version
+    }
+
+    fun getBluetoothSupportText():String{
+        if(AppContext.isBluetooth5Supported()){
+            return "Modern & Legacy"
+        } else {
+            return "Legacy Advertising only"
+        }
+    }
+
+    fun setupUi(){
+
+        // App Version
+        val textViewAppVersion: TextView = binding.startFragmentTextViewAppVersion
+        _viewModel!!.appVersion.observe(viewLifecycleOwner) {
+            textViewAppVersion.text = "App Version: $it"
+        }
+
+        // Android Version
+        val textViewAndroidVersion: TextView = binding.startFragmentTextViewAndroidVersion
+        _viewModel!!.androidVersion.observe(viewLifecycleOwner) {
+            textViewAndroidVersion.text = "Android Version: $it"
+        }
+
+        // SDK Version
+        val textViewSdkVersion: TextView = binding.startFragmentTextViewSdkVersion
+        _viewModel!!.sdkVersion.observe(viewLifecycleOwner) {
+            textViewSdkVersion.text = "SDK Version: $it"
+        }
+
+        // Bluetooth Support
+        val textViewBluetoothSupport: TextView = binding.startFragmentTextViewBluetooth
+        _viewModel!!.bluetoothSupport.observe(viewLifecycleOwner) {
+            textViewBluetoothSupport.text = "Bluetooth Support: $it"
+        }
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/de/simon/dankelmann/bluetoothlespam/ui/start/StartViewModel.kt
+++ app/src/main/java/de/simon/dankelmann/bluetoothlespam/ui/start/StartViewModel.kt
@@ -1,7 +1,26 @@
 package de.simon.dankelmann.bluetoothlespam.ui.start
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class StartViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    var appVersion = MutableLiveData<String>().apply {
+        value = "0.0.0"
+    }
+
+    var androidVersion = MutableLiveData<String>().apply {
+        value = "0"
+    }
+
+    var sdkVersion = MutableLiveData<String>().apply {
+        value = "0"
+    }
+
+    var bluetoothSupport = MutableLiveData<String>().apply {
+        value = "-"
+    }
+
+
 }
