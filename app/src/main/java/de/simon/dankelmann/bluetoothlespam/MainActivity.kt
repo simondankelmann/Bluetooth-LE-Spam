8122d1251b84f11ec2985ecfd7f2195f47024246
Simon <simon.dankelmann@gmail.com>
2023-11-20 18:46:35 +0100

    added tx power dialog

--- app/src/main/java/de/simon/dankelmann/bluetoothlespam/MainActivity.kt
+++ app/src/main/java/de/simon/dankelmann/bluetoothlespam/MainActivity.kt
@@ -1,6 +1,7 @@
 package de.simon.dankelmann.bluetoothlespam
 
 import android.Manifest
+import android.app.Dialog
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
@@ -15,6 +16,10 @@ import android.text.style.ForegroundColorSpan
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.Window
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -33,6 +38,7 @@ import com.google.android.material.navigation.NavigationView
 import de.simon.dankelmann.bluetoothlespam.AppContext.AppContext
 import de.simon.dankelmann.bluetoothlespam.Constants.Constants
 import de.simon.dankelmann.bluetoothlespam.Database.AppDatabase
+import de.simon.dankelmann.bluetoothlespam.Enums.TxPowerLevel
 import de.simon.dankelmann.bluetoothlespam.Helpers.BluetoothHelpers
 import de.simon.dankelmann.bluetoothlespam.Helpers.DatabaseHelpers
 import de.simon.dankelmann.bluetoothlespam.Helpers.QueueHandlerHelpers
@@ -146,13 +152,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onPrepareOptionsMenu(menu: Menu?): Boolean
     {
-        val actionSettingsMenuItem = menu?.findItem(R.id.nav_preferences)
-        val title = actionSettingsMenuItem?.title.toString()
-        val spannable = SpannableString(title)
+        val menuItems = listOf<MenuItem?>(menu?.findItem(R.id.nav_preferences), menu?.findItem(R.id.nav_set_tx_power))
 
-        val textColor = resources.getColor(R.color.text_color, AppContext.getContext().theme)
-        spannable.setSpan(ForegroundColorSpan(textColor), 0, spannable.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-        actionSettingsMenuItem?.title = spannable
+        menuItems.forEach { menuItem ->
+            val actionSettingsMenuItem = menuItem
+            val title = actionSettingsMenuItem?.title.toString()
+            val spannable = SpannableString(title)
+
+            val textColor = resources.getColor(R.color.text_color, AppContext.getContext().theme)
+            spannable.setSpan(ForegroundColorSpan(textColor), 0, spannable.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+            actionSettingsMenuItem?.title = spannable
+        }
 
         return super.onPrepareOptionsMenu(menu)
     }
@@ -174,8 +184,89 @@ class MainActivity : AppCompatActivity() {
                 val navController = findNavController(R.id.nav_host_fragment_content_main)
                 onNavDestinationSelected(item, navController)
             }
+
+            R.id.nav_set_tx_power -> {
+                showSetTxPowerDialog()
+            }
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    fun showSetTxPowerDialog(){
+
+        if(AppContext.getAdvertisementService() != null){
+
+            val dialog = Dialog(this)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.setContentView(R.layout.dialog_set_tx_power)
+
+            val seekBar:SeekBar = dialog.findViewById(R.id.setTxPowerDialogSeekbar)
+            val seekBarLabel:TextView = dialog.findViewById(R.id.setTxPowerDialogTxPowerTextView)
+
+            // Set Current TxPowerLevel
+            val currentTxPowerLevel = AppContext.getAdvertisementService().getTxPowerLevel()
+
+            val currentProgress = when(currentTxPowerLevel){
+                TxPowerLevel.TX_POWER_HIGH -> 3
+                TxPowerLevel.TX_POWER_MEDIUM -> 2
+                TxPowerLevel.TX_POWER_LOW -> 1
+                TxPowerLevel.TX_POWER_ULTRA_LOW -> 0
+            }
+
+            val currentLabel = when(currentTxPowerLevel){
+                TxPowerLevel.TX_POWER_HIGH -> "High"
+                TxPowerLevel.TX_POWER_MEDIUM -> "Medium"
+                TxPowerLevel.TX_POWER_LOW -> "Low"
+                TxPowerLevel.TX_POWER_ULTRA_LOW -> "Ultra Low"
+            }
+
+            seekBar.progress = currentProgress
+            seekBarLabel.text = currentLabel
+
+            seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+
+                    val newTxPowerLevel = when(progress){
+                        3 -> TxPowerLevel.TX_POWER_HIGH
+                        2 -> TxPowerLevel.TX_POWER_MEDIUM
+                        1 -> TxPowerLevel.TX_POWER_LOW
+                        0 -> TxPowerLevel.TX_POWER_ULTRA_LOW
+                        else -> TxPowerLevel.TX_POWER_HIGH
+                    }
+
+                    val newTxPowerLabel = when(progress){
+                        3 -> "High"
+                        2 -> "Medium"
+                        1 -> "Low"
+                        0 -> "Ultra Low"
+                        else -> "High"
+                    }
+
+                    if(AppContext.getAdvertisementService() != null){
+                        seekBarLabel.text = newTxPowerLabel
+                        AppContext.getAdvertisementService()!!.setTxPowerLevel(newTxPowerLevel)
+                    }
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) {
+                    // you can probably leave this empty
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
+                    // you can probably leave this empty
+                }
+            })
+
+            val okBtn = dialog.findViewById(R.id.setTxPowerDialogOkButton) as TextView
+            okBtn.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            dialog.show()
+        } else {
+            Toast.makeText(this, "Advertisement Service not initialized", Toast.LENGTH_SHORT)
+        }
     }
 }
