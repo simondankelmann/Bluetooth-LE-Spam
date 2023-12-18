53c028db648fcfc6bc1cf7565dd8ba4b278cf52f
Syuugo <pub@s1204.me>
2023-12-18 17:08:49 +0900

    Upgrade target version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
