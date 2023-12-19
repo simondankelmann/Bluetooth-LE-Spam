e4a0235eeecead8c144277d2d8499575088fb210
Simon <simon.dankelmann@gmail.com>
2023-12-19 18:28:13 +0100

    updated nav plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         google()
     }
     dependencies {
-        val nav_version = "2.7.5"
+        val nav_version = "2.7.6"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
     }
 }
