a15526801b093e75b3d2e49e52f47cb5542e7b64
Simon Dankelmann <simon.dankelmann@gmail.com>
2024-01-02 10:18:19 +0100

    raised version number, added apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.7"
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
