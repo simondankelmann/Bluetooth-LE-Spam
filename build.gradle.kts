56e3353072cef82c4427a6eb599a68ccf1616b40
Syuugo <pub@s1204.me>
2024-05-08 19:09:59 +0900

    Optimizes
    - Upgrade Gradle and AGP
    - Remove unused files
    - Update build workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
 }
 
 buildscript {
@@ -10,7 +10,7 @@ buildscript {
         google()
     }
     dependencies {
-        val nav_version = "2.7.6"
+        val nav_version = "2.7.7"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
     }
 }
