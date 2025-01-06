e8a173f187fd82016ababb576f6069e2e578a7d5
Thore Goebel <goebel.thore@gmail.com>
2025-01-06 12:30:53 +0100

    Prepare v1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "de.simon.dankelmann.bluetoothlespam"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.8"
+        versionCode = 3
+        versionName = "1.0.9"
 
         //testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
