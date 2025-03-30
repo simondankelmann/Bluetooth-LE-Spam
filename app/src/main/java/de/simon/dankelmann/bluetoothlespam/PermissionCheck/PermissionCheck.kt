c7d5b0210e1fcc21adeb4c0585b8cfe413a8bb69
Thore Goebel <goebel.thore@gmail.com>
2025-03-30 13:36:31 +0200

    Simplify checkPermission
    Don't manually implement this, don't try to maintain this list.
    Android should gracefully handle this, permissions that don't exist should return GRANTED.

--- app/src/main/java/de/simon/dankelmann/bluetoothlespam/PermissionCheck/PermissionCheck.kt
+++ app/src/main/java/de/simon/dankelmann/bluetoothlespam/PermissionCheck/PermissionCheck.kt
@@ -57,48 +57,8 @@ class PermissionCheck() {
         }
 
         fun checkPermission(permission: String, context: Context): Boolean {
-            if (permission == "android.permission.BLUETOOTH_ADVERTISE" && Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
-                // android.permission.BLUETOOTH_ADVERTISE was first introduced in api level 31
-                return true
-            }
-
-            if (permission == "android.permission.BLUETOOTH_CONNECT" && Build.VERSION.SDK_INT < Build.VERSION_CODES.S)
-            {
-                // android.permission.BLUETOOTH_CONNECT was first introduced in api level 31
-                return true
-            }
-
-            if (permission == "android.permission.BLUETOOTH_SCAN" && Build.VERSION.SDK_INT < Build.VERSION_CODES.S)
-            {
-                // android.permission.BLUETOOTH_CONNECT was first introduced in api level 31
-                return true
-            }
-
-            if ((permission == "android.permission.BLUETOOTH" || permission == "android.permission.BLUETOOTH_ADMIN") && Build.VERSION.SDK_INT > Build.VERSION_CODES.R)
-            {
-                // android.permission.BLUETOOTH and android.permission.BLUETOOTH_ADMIN have a max sdk version of 30
-                return true
-            }
-
-            if (permission == "android.permission.FOREGROUND_SERVICE_LOCATION" && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
-            {
-                // android.permission.FOREGROUND_SERVICE_LOCATION was first introduced in api level 29
-                return true
-            }
-
-            if (permission == "android.permission.ACCESS_BACKGROUND_LOCATION" && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
-            {
-                // android.permission.ACCESS_BACKGROUND_LOCATION was first introduced in api level 29
-                return true
-            }
-
-            if (permission == "android.permission.FOREGROUND_SERVICE_SPECIAL_USE" && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
-            {
-                // android.permission.FOREGROUND_SERVICE_SPECIAL_USE was first introduced in api level 29
-                return true
-            }
-
-            return ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+            val result = ContextCompat.checkSelfPermission(context, permission)
+            return result == PackageManager.PERMISSION_GRANTED
         }
     }
 }
